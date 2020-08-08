6a19d57d440561cdc812bb6332f3fc1734c6deee
Power5-sudo <67663157+Power5-sudo@users.noreply.github.com>
2020-08-08 20:37:50 +0800

    添加项目文件。

---
+++ Log.h
@@ -0,0 +1,11 @@
+#ifndef _LOG_H
+#define _LOG_H
+using namespace std;
+
+int Multiply(int a, int b);
+
+void MultiplyAndLog(int a, int b);
+
+struct Player {};
+
+#endif
---
+++ MALOG.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Log.h"
+
+
+
+void MultiplyAndLog(int a, int b)
+{
+    int result = Multiply(a, b);
+    cout << result << endl;
+}
